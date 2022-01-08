aeaf43380911609bddc96b11bb62c8c766995b1f
Kenta Mukai <r@muk.ai>
2022-01-08 17:18:48 +0900

    :sparkles: display path instead of debug print

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ fn main() -> std::io::Result<()> {
     let target_dir = env::current_dir()?;
     let picked_file = random_pick(&target_dir);
     match picked_file {
-        Some(path) => println!("{:?}", path),
+        Some(path) => match path.to_str() {
+            Some(path_str) => println!("{}", path_str),
+            None => panic!("invalid unicode file name: {:?}", path),
+        },
         None => panic!("no files"),
     }
     Ok(())
